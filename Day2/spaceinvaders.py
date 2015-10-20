4f12821679c331c821e5bdf9e18c0d57132665bd
Yun Zhou <yzhou@palantir.com>
2015-10-19 23:36:56 -0700

    Complete game

--- Day2/spaceinvaders.py
+++ Day2/spaceinvaders.py
@@ -20,7 +20,7 @@ def isPointInsideRect(x, y, rect):
     else:
         return False
 
-import pygame, sys
+import pygame, sys, random
 pygame.init()
 
 SCREEN_WIDTH = 640
@@ -29,7 +29,8 @@ SCREEN_HEIGHT = 480
 screen = pygame.display.set_mode([SCREEN_WIDTH,SCREEN_HEIGHT])
 
 # List of missiles flying across the screen
-missiles = []
+player_missiles = []
+alien_missiles = []
 black = [0, 0, 0]
 white = [255, 255, 255]
 
@@ -48,9 +49,12 @@ class Spaceship:
   def moveLeft(self):
     self.xpos -= 5
 
-  def fire(self):
-    missile = Missile(self.xpos, self.ypos - 32, -10)
-    missiles.append(missile)
+  def shoot(self):
+    missile = Missile(self.xpos + 16, self.ypos - 1, -10)
+    player_missiles.append(missile)
+
+  def get_rect(self):
+    return pygame.Rect(self.xpos, self.ypos, 32, 32)
 
   def draw(self):
     screen.blit(self.picture, (self.xpos, self.ypos))
@@ -59,11 +63,29 @@ class Alien:
   xpos = 0
   ypos = 0
   xspeed = 2
-  picture = pygame.image.load('alien1.png')
+  picture1 = pygame.image.load('alien1.png')
+  picture2 = pygame.image.load('alien2.png')
 
   def __init__(self, x, y):
     self.xpos = x
     self.ypos = y
+
+  def move(self, x, y):
+    self.xpos += x
+    self.ypos += y
+    self.picture3 = self.picture1
+    self.picture1 = self.picture2
+    self.picture2 = self.picture3
+
+  def shoot(self):
+    missile = Missile(self.xpos + 16, self.ypos + 32, 10)
+    alien_missiles.append(missile)
+
+  def get_rect(self):
+    return pygame.Rect(self.xpos, self.ypos, 32, 32)
+
+  def draw(self):
+    screen.blit(self.picture1, (self.xpos, self.ypos))
 
 
 class Missile:
@@ -85,13 +107,23 @@ class Missile:
     if self.ypos < 0 or self.ypos > SCREEN_HEIGHT:
       self.spent = True
 
+  def get_rect(self):
+    return pygame.Rect(self.xpos, self.ypos, 2, 10)
+
   def isSpent(self):
     return self.spent
 
 # Create all the objects and variables!
 spaceship = Spaceship(200, 440)
 aliens = []
+for x in range(1,8):
+  for y in range(1,4):
+    aliens.append(Alien(x * 50 + 10, y * 50 + 10))
+
 running = True
+ticks = 0
+alien_formation_x = 0
+alien_formation_x_speed = 10
 
 # Main event loop. Here we:
 # 1. Handle keyboard input for the player and move the player around
@@ -109,21 +141,71 @@ while running:
   if keys[pygame.K_LEFT]:
       spaceship.moveLeft()
   if keys[pygame.K_SPACE]:
-      spaceship.fire()
+      spaceship.shoot()
 
   screen.fill(black)
 
-  # Move and draw the missiles
-  for missile in missiles:
+  # Move, detect collisions, and draw the player's missiles
+  for missile in player_missiles:
     missile.move()
+    for alien in aliens:
+      if doRectsOverlap(alien.get_rect(), missile.get_rect()):
+        aliens.remove(alien)
+        missile.spent = True
+
     if missile.isSpent():
-      missiles.remove(missile)
+      player_missiles.remove(missile)
+    missile.draw()
+
+  # Move, detect collisions, and draw the player's missiles
+  for missile in alien_missiles:
+    missile.move()
+
+    if doRectsOverlap(spaceship.get_rect(), missile.get_rect()):
+      print "YOU LOSE"
+      running = False
+      missile.spent = True
+
+    if missile.isSpent():
+      alien_missiles.remove(missile)
     missile.draw()
 
   # Move all the aliens
+  # The more aliens there are, the slower they move
+  if ticks % (len(aliens) + 1) == 0:
+    # The alien formation moves left and right
+    alien_formation_x += alien_formation_x_speed
+    for alien in aliens:
+      alien.move(alien_formation_x_speed, 0)
+    # Until the formation reaches a limit, after which it reverses
+    # direction and moves down 5 pixels
+    if alien_formation_x >= 200 or alien_formation_x <= 0:
+      alien_formation_x_speed = -1 * alien_formation_x_speed
+      for alien in aliens:
+        alien.move(0, 5)
+
+  # Draw all the aliens and give them a chance to shoot!
+  for alien in aliens:
+    alien.draw()
+    # Aliens shoot randomly
+    # 0.05 percent change per frame that an alien shoots
+    if (random.randint(1,1000) < 5):
+      alien.shoot()
+
+  # If there are no more aliens, the player wins
+  if len(aliens) == 0:
+    print "YOU WIN!"
+    running = False
+
+  # If any alien reaches the bottom of the screen, the player loses
+  for alien in aliens:
+    if alien.ypos > SCREEN_HEIGHT - 64:
+      print "YOU LOSE"
+      running = False
 
   # Draw the spaceship
   spaceship.draw()
   pygame.display.update()
+  ticks = ticks + 1
 
 pygame.quit()
