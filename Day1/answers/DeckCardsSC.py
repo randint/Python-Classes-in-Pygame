556ec0e031a71d61b91508b21c1e5cd2fb555596
randint <xkcdrom@gmail.com>
2016-01-29 15:11:32 -0800

    Improve DeckCards solution

--- Day1/answers/DeckCardsSC.py
+++ Day1/answers/DeckCardsSC.py
@@ -23,62 +23,42 @@ values = {
     "K": "King",
 }
 
+
 class Card:
     def __init__(self, suit, value):
         self.suit = suit
         self.value = value
 
-    def getSuit(self):
+    def get_suit(self):
         return self.suit
 
-    def getValue(self):
+    def get_value(self):
         return self.value
 
     def __str__(self):
         return values.get(self.value) + " of " + suits.get(self.suit)
+
 
 class Deck:
     def __init__(self):
         self.deck = []
         self.dealtCards = []
 
-        for s in range(4):
-            suit, value = "", ""
-            if s == 0:
-                suit = "H"
-            elif s == 1:
-                suit = "S"
-            elif s == 2:
-                suit = "C"
-            elif s == 3:
-                suit = "D"
-            for v in range(1, 14):
-                if v == 1:
-                    value = "A"
-                elif v <= 10:
-                    value = str(v)
-                elif v == 11:
-                    value = "J"
-                elif v == 12:
-                    value = "Q"
-                elif v == 13:
-                    value = "K"
+        for suit in ['H', 'S', 'D', 'C']:
+            for value in ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']:
                 self.deck.append(Card(suit, value))
 
-    def dealCard(self):
-        picked = self.deck[random.randint(0, len(self.deck)) - 1]
-        print(picked.value, picked.suit)
-        self.dealtCards.append(picked)
-        self.deck.remove(picked)
+    def deal_card(self):
+        self.dealtCards.append(self.deck.pop(random.randint(0, len(self.deck)) - 1))
         return self.dealtCards[-1]
 
-    def numCardsRemaining(self):
+    def num_remaining(self):
         return len(self.deck)
 
-deck = Deck()
 
+deck = Deck()
 for card in deck.deck:
     print(card)
+
 while deck.deck:
-    print(deck.dealCard())
-print(deck.deck)
+    print(deck.deal_card())
